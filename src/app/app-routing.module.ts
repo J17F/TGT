0addf8c29e5cc8b47b7b1427455468a96fdbce0f
J17F <josefgomezg17@gmail.com>
2023-03-24 20:07:14 -0400

    modificaciones de help y contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { ContentComponent } from './pages/content/content.component';
 import { HelpComponent } from './pages/help/help.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about',component: AboutComponent},
-  { path: 'contact', component: ContactComponent},
   { path: 'content', component: ContentComponent},
   { path: 'help', component: HelpComponent},
   { path: '**', pathMatch: 'full', redirectTo: ''}
